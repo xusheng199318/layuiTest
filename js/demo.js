e7892c1f566aed6edf692c3b8f8c363f3010556a
arthur <465034663@qq.com>
2019-05-28 22:33:23 +0800

    Initial commit

---
+++ js/demo.js
@@ -0,0 +1,7 @@
+layui.define(function(exports) {
+
+    alert("haha");
+    exports('demo', function () {
+        console.log("demo");
+    });
+});
---
+++ view/demo.js
@@ -0,0 +1,7 @@
+layui.define(function(exports) {
+
+    console.log("haha");
+    exports('demo', function () {
+        console.log("demo");
+    });
+});
